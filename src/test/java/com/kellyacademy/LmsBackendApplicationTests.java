ded6843ab8af54e37f47860767c600b35a2b823b
MedinaDiaz03 <I.indetectableAI@gmail.com>
2026-09-12 14:55:49 -0500

    Proyecto base Spring Boot para la academia

---
+++ src/test/java/com/kellyacademy/LmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellyacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
